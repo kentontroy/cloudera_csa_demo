6a2b855e29f39a3fe6f52d34f01a9cabf7c5f022
Kenton Troy <kentontroy@yahoo.com>
2022-03-08 13:16:54 -0500

    Update jupyter.sh

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 
-WORKDIR=/home/ec2-user/demo/dev/docker
-
-docker run -it -p 8888:8888 -v ${WORKDIR}:/home/jovyan/work --rm --name jupyter jupyter/datascience-notebook
+docker run -it -p 8888:8888 -v ${CML_DEMO_DIR}:/home/jovyan/work --rm --name jupyter jupyter/datascience-notebook
